89b0c072190d2f47bc29ca0b69487f77655a7a73
Suraj Nag <suraj11nag@gmail.com>
2024-01-06 16:21:22 +0530

    update

---
+++ etl_twitter.py
@@ -0,0 +1,12 @@
+import tweepy
+import pandas
+import json
+from datetime import datetime
+import s3fs
+
+access_key = "qpflF7ioTxUxgHQo5lvzUpGSi"
+access_secret = "tJyWLssqEuKSIIeMeVxsiykHdCDFmEXBeKotc9IxI0rYMdKpOr"
+consumer_key = "756422560468926464-m0VRPpibk2EpYY8FwzyTF7Qeg6xo5zN"
+consumer_secret = "jP1kr9zGQWIiov5H39FtZ9b10jsXlZprg08HzBEKIm6qp"
+
+#twitter
